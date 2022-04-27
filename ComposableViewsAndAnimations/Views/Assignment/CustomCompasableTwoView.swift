73b423eaf15160e40d28c2852b97d3e707739733
Nathan Smith <nsmith@lcs.on.ca>
2022-04-27 12:23:09 -0400

    thanks to rule of three, the image now tracks to the slider

--- ComposableViewsAndAnimations/Views/Assignment/CustomCompasableTwoView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomCompasableTwoView.swift
@@ -11,22 +11,34 @@ struct CustomCompasableTwoView: View {
     
     @State var input: Double = 30.0
     
+    @State var animationOpacity: Bool = true
+    
     var formattedinput: String {
         return String(format: "%.0f", input)
     }
     
     var body: some View {
         VStack {
+            ZStack {
+                Image(systemName: "star.circle.fill")
+                    .resizable()
+                    .foregroundColor(.yellow)
+                    .frame(width: 35, height: 35)
+                    .opacity(animationOpacity ? 1.0 : 0.0)
+                    .offset(x: (input - 30) * 140 / 30, y: 0.0)
+                
+                HStack {
+                    Text("00")
+                    Slider(value: $input, in: 0.0...60.0, label: {
+                        Text("Speed")
+                    })
+                    Text("60")
+                }
+                .padding(.horizontal)
+            }
+            
             Text("\(formattedinput)")
             
-            HStack {
-                Text("0")
-                Slider(value: $input, in: 0.0...60.0, label: {
-                    Text("Speed")
-                })
-                Text("60")
-            }
-            .padding(.horizontal)
         }
     }
 }
