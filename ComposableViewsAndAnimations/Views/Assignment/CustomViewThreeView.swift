d002400686371ce4a6495b60825fed67d9da7f11
Nathan Smith <nsmith@lcs.on.ca>
2022-04-28 11:33:18 -0400

    new view for new animation

---
+++ ComposableViewsAndAnimations/Views/Assignment/CustomViewThreeView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomViewThreeView.swift
+//  ComposableViewsAndAnimations
+//
+//  Created by Nathan Smith on 2022-04-28.
+//
+
+import SwiftUI
+
+struct CustomViewThreeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomViewThreeView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomViewThreeView()
+    }
+}
